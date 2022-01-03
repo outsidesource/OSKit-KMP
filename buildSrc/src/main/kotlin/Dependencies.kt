73f23fee11350904bd85c47858d33bb53b78567f
Ryan Mitchener <ryan@outsidesource.com>
2022-01-03 14:38:13 -0500

    Updated libraries

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val Kotlin = "1.5.31"
-    const val ComposePlugin = "1.0.0"
-    const val KotlinCompilerExtension = "1.0.1"
+    const val Kotlin = "1.6.10"
+    const val ComposePlugin = "1.0.1"
+    const val KotlinCompilerExtension = "1.1.0-rc2"
 
-    const val CoroutinesCore = "1.5.2"
+    const val CoroutinesCore = "1.6.0"
     const val AndroidXCore = "1.6.0"
     const val AndroidXActivityCompose = "1.3.1"
     const val AndroidXLifecycleViewModelCompose = "1.0.0-alpha07"
     const val KotlinxDateTime = "0.3.0"
-    const val KotlinxAtomicFu = "0.16.3"
+    const val KotlinxAtomicFu = "0.17.0"
 }
 
 object Dependencies {
