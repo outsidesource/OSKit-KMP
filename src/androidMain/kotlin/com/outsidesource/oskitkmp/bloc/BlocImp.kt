9382e58db53ca5c8669bedff07ff52bc6419b5df
Ryan Mitchener <ryan@outsidesource.com>
2022-05-03 12:26:10 -0400

    Added ability for rememberBloc to use keys

--- src/androidMain/kotlin/com/outsidesource/oskitkmp/bloc/BlocImp.kt
+++ src/androidMain/kotlin/com/outsidesource/oskitkmp/bloc/BlocImp.kt
@@ -1,28 +1,98 @@
 package com.outsidesource.oskitkmp.bloc
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 internal actual val defaultBlocDispatcher: CoroutineDispatcher = Dispatchers.Default
 
 internal class BlocViewModel : ViewModel()
 
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for a given lifetime.
+ * The lifetime for the Bloc is the life of the route NOT the life of the composable.
+ */
 @Composable
-fun <B : Bloc<S>, S> rememberBloc(factory: () -> B): Pair<S, B> {
-    val viewModel = viewModel<BlocViewModel>()
-    val (bloc, stream) = remember {
-        val bloc = factory()
-        val stream = bloc.stream(viewModel.viewModelScope)
+fun <B : Bloc<S>, S> rememberBloc(factory: () -> B): Pair<S, B> = rememberBloc(
+    rememberFactory = { remember { it() } },
+    blocFactory = factory,
+)
+
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for the lifetime of the keys provided.
+ */
+@Composable
+fun <B : Bloc<S>, S> rememberBloc(
+    key1: Any? = Unit,
+    factory: () -> B,
+): Pair<S, B> {
+    val scope = remember(key1) { CoroutineScope(Dispatchers.Default + Job()) }
+    DisposableEffect(scope) { onDispose { scope.cancel() } }
+
+    return rememberBloc(
+        rememberFactory = { remember(key1) { it() } },
+        blocFactory = factory,
+        scope = scope,
+    )
+}
+
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for the lifetime of the keys provided.
+ */
+@Composable
+fun <B : Bloc<S>, S> rememberBloc(
+    key1: Any? = Unit,
+    key2: Any? = Unit,
+    factory: () -> B,
+): Pair<S, B> {
+    val scope = remember(key1, key2) { CoroutineScope(Dispatchers.Default + Job()) }
+    DisposableEffect(scope) { onDispose { scope.cancel() } }
+
+    return rememberBloc(
+        rememberFactory = { remember(key1, key2) { it() } },
+        blocFactory = factory,
+        scope = scope,
+    )
+}
+
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for the lifetime of the keys provided.
+ */
+@Composable
+fun <B : Bloc<S>, S> rememberBloc(
+    key1: Any? = Unit,
+    key2: Any? = Unit,
+    key3: Any? = Unit,
+    factory: () -> B,
+): Pair<S, B> {
+    val scope = remember(key1, key2) { CoroutineScope(Dispatchers.Default + Job()) }
+    DisposableEffect(scope) { onDispose { scope.cancel() } }
+
+    return rememberBloc(
+        rememberFactory = { remember(key1, key2, key3) { it() } },
+        blocFactory = factory,
+        scope = scope
+    )
+}
+
+@Composable
+private fun <B : Bloc<S>, S> rememberBloc(
+    rememberFactory: @Composable (@DisallowComposableCalls () -> Pair<B, Flow<S>>) -> Pair<B, Flow<S>>,
+    blocFactory: () -> B,
+    scope: CoroutineScope? = null,
+): Pair<S, B> {
+    val streamScope = scope ?: viewModel<BlocViewModel>().viewModelScope
+
+    val (bloc, stream) = rememberFactory {
+        val bloc = blocFactory()
+        val stream = bloc.stream(streamScope)
         Pair(bloc, stream)
     }
+
     val state by stream.collectAsState(initial = bloc.state)
     return Pair(state, bloc)
 }
--- src/jvmMain/kotlin/com/outsidesource/oskitkmp/bloc/BlocImp.kt
+++ src/jvmMain/kotlin/com/outsidesource/oskitkmp/bloc/BlocImp.kt
@@ -1,26 +1,96 @@
 package com.outsidesource.oskitkmp.bloc
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import com.outsidesource.oskitkmp.router.LocalRouteScope
 import com.outsidesource.oskitkmp.router.RouteDestroyedEffect
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 internal actual val defaultBlocDispatcher: CoroutineDispatcher = Dispatchers.Default
 
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for a given lifetime.
+ * The lifetime for the Bloc is the life of the route NOT the life of the composable.
+ */
 @Composable
-fun <B : Bloc<S>, S> rememberBloc(factory: () -> B): Pair<S, B> {
-    val routeScope = LocalRouteScope.current
-    RouteDestroyedEffect { routeScope.cancel() }
+fun <B : Bloc<S>, S> rememberBloc(factory: () -> B): Pair<S, B> = rememberBloc(
+    rememberFactory = { remember { it() } },
+    blocFactory = factory,
+)
 
-    val (bloc, stream) = remember {
-        val bloc = factory()
-        val stream = bloc.stream(routeScope)
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for the lifetime of the keys provided.
+ */
+@Composable
+fun <B : Bloc<S>, S> rememberBloc(
+    key1: Any? = Unit,
+    factory: () -> B,
+): Pair<S, B> {
+    val scope = remember(key1) { CoroutineScope(Dispatchers.Default + Job()) }
+    DisposableEffect(scope) { onDispose { scope.cancel() } }
+
+    return rememberBloc(
+        rememberFactory = { remember(key1) { it() } },
+        blocFactory = factory,
+        scope = scope,
+    )
+}
+
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for the lifetime of the keys provided.
+ */
+@Composable
+fun <B : Bloc<S>, S> rememberBloc(
+    key1: Any? = Unit,
+    key2: Any? = Unit,
+    factory: () -> B,
+): Pair<S, B> {
+    val scope = remember(key1, key2) { CoroutineScope(Dispatchers.Default + Job()) }
+    DisposableEffect(scope) { onDispose { scope.cancel() } }
+
+    return rememberBloc(
+        rememberFactory = { remember(key1, key2) { it() } },
+        blocFactory = factory,
+        scope = scope,
+    )
+}
+
+/**
+ * rememberBloc will remember a Bloc and subscribe to its state for the lifetime of the keys provided.
+ */
+@Composable
+fun <B : Bloc<S>, S> rememberBloc(
+    key1: Any? = Unit,
+    key2: Any? = Unit,
+    key3: Any? = Unit,
+    factory: () -> B,
+): Pair<S, B> {
+    val scope = remember(key1, key2) { CoroutineScope(Dispatchers.Default + Job()) }
+    DisposableEffect(scope) { onDispose { scope.cancel() } }
+
+    return rememberBloc(
+        rememberFactory = { remember(key1, key2, key3) { it() } },
+        blocFactory = factory,
+        scope = scope
+    )
+}
+
+@Composable
+private fun <B : Bloc<S>, S> rememberBloc(
+    rememberFactory: @Composable (@DisallowComposableCalls () -> Pair<B, Flow<S>>) -> Pair<B, Flow<S>>,
+    blocFactory: () -> B,
+    scope: CoroutineScope? = null,
+): Pair<S, B> {
+    val streamScope = scope ?: run {
+        val routeScope = LocalRouteScope.current
+        RouteDestroyedEffect { routeScope.cancel() }
+        routeScope
+    }
+
+    val (bloc, stream) = rememberFactory {
+        val bloc = blocFactory()
+        val stream = bloc.stream(streamScope)
         Pair(bloc, stream)
     }
 
