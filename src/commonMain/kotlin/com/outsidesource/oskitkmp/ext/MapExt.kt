2974fb60807fb884b6c3fb32d6106f02b10c62ef
Ryan Mitchener <ryan@outsidesource.com>
2023-02-02 13:55:24 -0500

    Updated MapExt based on lint

--- src/commonMain/kotlin/com/outsidesource/oskitkmp/ext/MapExt.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitkmp/ext/MapExt.kt
@@ -1,6 +1,6 @@
 package com.outsidesource.oskitkmp.ext
 
-inline fun <K, V> MutableMap<K, V>.putIfAbsent(key: K, value: V): V {
+fun <K, V> MutableMap<K, V>.putIfAbsent(key: K, value: V): V {
     val v = get(key)
     if (v == null) {
         put(key, value)
