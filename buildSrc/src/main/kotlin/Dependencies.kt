d79a76dcb82c500d36c34efdad04d9101db18639
Ryan Mitchener <ryan@outsidesource.com>
2023-06-04 21:50:13 -0400

    Updated dependencies and version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,23 +1,19 @@
 object Versions {
     const val Kotlin = "1.8.0"
-    const val KotlinCompilerExtension = "1.4.0"
     const val KtLintPlugin = "10.2.1"
 
-    const val CoroutinesCore = "1.6.4"
-    const val AndroidXCore = "1.9.0"
+    const val CoroutinesCore = "1.7.1"
     const val KotlinxDateTime = "0.4.0"
-    const val KotlinxAtomicFu = "0.19.0"
-    const val KotlinxSerializationJson = "1.5.0-RC"
-    const val Ktor = "2.2.2"
+    const val KotlinxAtomicFu = "0.20.2"
+    const val KotlinxSerializationJson = "1.5.1"
+    const val Ktor = "2.3.1"
 }
 
 object Dependencies {
     const val CoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.CoroutinesCore}"
-    const val AndroidXCore = "androidx.core:core-ktx:${Versions.AndroidXCore}"
     const val KotlinxDateTime = "org.jetbrains.kotlinx:kotlinx-datetime:${Versions.KotlinxDateTime}"
     const val KotlinxAtomicFu = "org.jetbrains.kotlinx:atomicfu:${Versions.KotlinxAtomicFu}"
     const val KotlinxSerializationJson = "org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.KotlinxSerializationJson}"
-    const val KtorNetwork = "io.ktor:ktor-network:${Versions.Ktor}"
     const val KtorWebsockets = "io.ktor:ktor-server-websockets:${Versions.Ktor}"
     const val KtorServerCore = "io.ktor:ktor-server-core:${Versions.Ktor}"
     const val KtorServerCIO = "io.ktor:ktor-server-cio:${Versions.Ktor}"
