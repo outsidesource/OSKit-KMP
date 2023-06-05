da5e4bd0bc1fcbf43ccda7afe5bd4648c907cbe7
Ryan Mitchener <ryan@outsidesource.com>
2023-06-04 21:50:45 -0400

    Updated createCoordinatorObserver to be a companion object

--- src/commonMain/kotlin/com/outsidesource/oskitkmp/coordinator/Coordinator.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitkmp/coordinator/Coordinator.kt
@@ -14,6 +14,7 @@ abstract class Coordinator(
         get() = router.routeStack
     protected val current
         get() = router.current
+
     protected fun hasBackStack() = router.hasBackStack()
 
     protected fun push(route: IRoute, transition: IRouteTransition? = null, force: Boolean = false) =
@@ -22,7 +23,7 @@ abstract class Coordinator(
     protected fun replace(route: IRoute, transition: IRouteTransition? = null, force: Boolean = false) =
         router.replace(route, transition, force)
 
-    protected fun pop(force: Boolean = false) = router.pop(force)
+    fun pop(force: Boolean = false) = router.pop(force)
 
     protected fun popWhile(force: Boolean = false, block: (entry: IRoute) -> Boolean) =
         router.popWhile(force, block)
@@ -32,22 +33,24 @@ abstract class Coordinator(
 
     protected fun popTo(to: IRoute, inclusive: Boolean = false, force: Boolean = false) =
         router.popTo(to, inclusive, force)
-}
 
-fun createCoordinatorObserver(coordinator: Coordinator): ICoordinatorObserver =
-    object : ICoordinatorObserver {
-        override fun hasBackStack() = coordinator.router.hasBackStack()
+    companion object {
+        fun createObserver(coordinator: Coordinator): ICoordinatorObserver =
+            object : ICoordinatorObserver {
+                override fun hasBackStack() = coordinator.router.hasBackStack()
 
-        override fun pop() = coordinator.router.pop()
+                override fun pop() = coordinator.router.pop()
 
-        override val routeFlow: StateFlow<RouteStackEntry> = coordinator.router.routeFlow
+                override val routeFlow: StateFlow<RouteStackEntry> = coordinator.router.routeFlow
 
-        override fun markTransitionStatus(status: RouteTransitionStatus) =
-            coordinator.router.markTransitionStatus(status)
+                override fun markTransitionStatus(status: RouteTransitionStatus) =
+                    coordinator.router.markTransitionStatus(status)
 
-        override fun addRouteDestroyedListener(block: () -> Unit) =
-            coordinator.router.addRouteDestroyedListener(block)
+                override fun addRouteDestroyedListener(block: () -> Unit) =
+                    coordinator.router.addRouteDestroyedListener(block)
+            }
     }
+}
 
 interface ICoordinatorObserver {
     fun hasBackStack(): Boolean
