7c386271ececa29f71d6d06d4bc04168e8bece97
Ryan Mitchener <ryan@outsidesource.com>
2024-10-03 22:49:58 -0400

    Added NoOp permission implementations for desktop

--- src/jvmMain/kotlin/com/outsidesource/oskitkmp/capability/BluetoothCapability.jvm.kt
+++ src/jvmMain/kotlin/com/outsidesource/oskitkmp/capability/BluetoothCapability.jvm.kt
@@ -2,41 +2,32 @@ package com.outsidesource.oskitkmp.capability
 
 import com.outsidesource.oskitkmp.outcome.Outcome
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
+/**
+ * TODO: Implement this properly for MacOS, Linux, and Windows
+ */
 class BluetoothCapability(private val flags: Array<BluetoothCapabilityFlags>): IInitializableCapability, ICapability {
-    override fun init(context: CapabilityContext) {
-        TODO("Not yet implemented")
-    }
+    override fun init(context: CapabilityContext) {}
 
-    override val status: CapabilityStatus
-        get() = TODO("Not yet implemented")
-    override val statusFlow: Flow<CapabilityStatus>
-        get() = TODO("Not yet implemented")
-    override val hasPermissions: Boolean
-        get() = TODO("Not yet implemented")
-    override val hasEnablableService: Boolean
-        get() = TODO("Not yet implemented")
-    override val supportsRequestEnable: Boolean
-        get() = TODO("Not yet implemented")
-    override val supportsOpenAppSettingsScreen: Boolean
-        get() = TODO("Not yet implemented")
-    override val supportsOpenEnableSettingsScreen: Boolean
-        get() = TODO("Not yet implemented")
+    override val status: CapabilityStatus = CapabilityStatus.Ready
+    override val statusFlow: Flow<CapabilityStatus> = flowOf(CapabilityStatus.Ready)
 
-    override suspend fun requestPermissions(): Outcome<CapabilityStatus, Any> {
-        TODO("Not yet implemented")
-    }
+    override val hasPermissions: Boolean = false
+    override val hasEnablableService: Boolean = false
+    override val supportsRequestEnable: Boolean = false
+    override val supportsOpenAppSettingsScreen: Boolean = false
+    override val supportsOpenEnableSettingsScreen: Boolean = false
 
-    override suspend fun requestEnable(): Outcome<CapabilityStatus, Any> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun requestPermissions(): Outcome<CapabilityStatus, Any> =
+        Outcome.Ok(CapabilityStatus.Ready)
 
-    override suspend fun openEnableSettingsScreen(): Outcome<Unit, Any> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun requestEnable(): Outcome<CapabilityStatus, Any> =
+        Outcome.Error(CapabilityServiceError.UnsupportedOperation)
 
-    override suspend fun openAppSettingsScreen(): Outcome<Unit, Any> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun openEnableSettingsScreen(): Outcome<Unit, Any> =
+        Outcome.Error(CapabilityServiceError.UnsupportedOperation)
 
+    override suspend fun openAppSettingsScreen(): Outcome<Unit, Any> =
+        Outcome.Error(CapabilityServiceError.UnsupportedOperation)
 }
