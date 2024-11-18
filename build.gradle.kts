41a6105917a5bfbf8dec4bf150fe8ee11dc07a4d
Ryan Mitchener <ryan@outsidesource.com>
2024-11-17 21:40:49 -0500

    Minor updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinMultiplatform
 import com.vanniktech.maven.publish.SonatypeHost
 import java.io.FileInputStream
