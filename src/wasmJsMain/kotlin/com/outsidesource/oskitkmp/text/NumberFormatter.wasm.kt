768d42c8912b35221b170ae752528ec5f79b3ad7
Ryan Mitchener <ryan@outsidesource.com>
2024-11-22 23:46:25 -0500

    Implemented NumberFormatter for WASM

--- src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/text/NumberFormatter.wasm.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/text/NumberFormatter.wasm.kt
@@ -5,11 +5,43 @@ actual class NumberFormatter actual constructor(
     private val maximumFractionDigits: Int,
     private val useGrouping: Boolean,
 ) {
+    private val formatter = Intl.NumberFormat(
+        numberFormatOptions(
+            minimumFractionDigits = minimumFractionDigits,
+            maximumFractionDigits = maximumFractionDigits,
+            useGrouping = useGrouping,
+        ),
+    )
 
-    actual fun format(value: Float): String = ""
-    actual fun format(value: Double): String = ""
-    actual fun format(value: Int): String = ""
-    actual fun format(value: Long): String = ""
+    actual fun format(value: Float): String = formatter.format(value)
+    actual fun format(value: Double): String = formatter.format(value)
+    actual fun format(value: Int): String = formatter.format(value)
+    actual fun format(value: Long): String = formatter.format(value)
 
     actual companion object
 }
+
+private external object Intl {
+    class NumberFormat(options: JsAny?) {
+        constructor(locales: String, options: JsAny?)
+
+        fun format(value: Float): String
+        fun format(value: Double): String
+        fun format(value: Int): String
+        fun format(value: Long): String
+    }
+}
+
+private fun numberFormatOptions(
+    minimumFractionDigits: Int,
+    maximumFractionDigits: Int,
+    useGrouping: Boolean,
+): JsAny = js(
+    """
+    ({
+        "minimumFractionDigits": minimumFractionDigits,
+        "maximumFractionDigits": maximumFractionDigits,
+        "useGrouping": useGrouping,
+    })
+""",
+)
