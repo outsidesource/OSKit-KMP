ce41cb96bee22ea649561448fd1dff4a2a9e5d63
Ryan Mitchener <ryan@outsidesource.com>
2024-11-25 02:26:31 -0500

    Continued work on IndexedDb

--- src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/lib/JsExt.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/lib/JsExt.kt
@@ -1,5 +1,29 @@
 package com.outsidesource.oskitkmp.lib
 
+import com.outsidesource.oskitkmp.outcome.Outcome
+
 fun List<String>.toJsArray(): JsArray<JsString> = JsArray<JsString>().apply {
     this@toJsArray.forEachIndexed { i, string -> set(i, string.toJsString()) }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T : JsAny> jsTryOutcome(block: () -> T): Outcome<T, Any> {
+    val result = jsTry(block = block)
+    result.error?.let { return Outcome.Error(it) }
+    return Outcome.Ok(result as T)
+}
+
+fun <T : JsAny> jsTry(block: () -> T): JsResult<T> = js(
+    """{
+        try {
+            return { result: block() }
+        } catch (e) {
+            return { error: e }
+        }
+    }""",
+)
+
+external interface JsResult<T : JsAny> : JsAny {
+    val error: JsAny?
+    val result: T?
 }
---
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/storage/IndexedDbExternal.kt
@@ -0,0 +1,185 @@
+package com.outsidesource.oskitkmp.storage
+
+import com.outsidesource.oskitkmp.lib.jsTryOutcome
+import com.outsidesource.oskitkmp.outcome.Outcome
+import com.outsidesource.oskitkmp.outcome.runOnError
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+external object indexedDB {
+    fun open(name: String): IDBOpenDBRequest
+    fun open(name: String, version: Int): IDBOpenDBRequest
+}
+
+external class IDBOpenDBRequest : JsAny {
+    var onerror: (Event) -> Unit
+    var onblocked: (Event) -> Unit
+    var onsuccess: (IDBOpenSuccessEvent) -> Unit
+    var onupgradeneeded: (IDBVersionChangeEvent) -> Unit
+    val result: IDBDatabase
+}
+
+suspend fun IDBOpenDBRequest.await(
+    onUpgradeNeeded: (IDBDatabase, oldVersion: Int, newVersion: Int) -> Unit,
+): Outcome<IDBDatabase, Any> = suspendCoroutine { continuation ->
+    onerror = { continuation.resume(Outcome.Error(it)) }
+    onsuccess = { continuation.resume(Outcome.Ok(it.target.result)) }
+    onblocked = { continuation.resume(Outcome.Error(it)) }
+    onupgradeneeded = {
+        jsTryOutcome {
+            onUpgradeNeeded(it.target.result, it.oldVersion, it.newVersion)
+            (0).toJsNumber()
+        }.runOnError {
+            continuation.resume(Outcome.Error(it))
+        }
+    }
+}
+
+external class IDBRequest<T : JsAny> : JsAny {
+    val error: JsAny
+    val readyState: JsAny
+    val source: JsAny
+    val transaction: JsAny
+    var onerror: (Event) -> Unit
+    var onsuccess: (Event) -> Unit
+    val result: T
+}
+
+suspend fun IDBRequest<JsString>.await(): Outcome<String, Any> = suspendCoroutine { continuation ->
+    onsuccess = {
+        jsTryOutcome {
+            continuation.resume(Outcome.Ok(result.toString()))
+            (0).toJsNumber()
+        }.runOnError {
+            continuation.resume(Outcome.Error(it))
+        }
+    }
+    onerror = { continuation.resume(Outcome.Error(it)) }
+}
+
+suspend fun IDBRequest<JsNumber>.await(): Outcome<Int, Any> = suspendCoroutine { continuation ->
+    onsuccess = {
+        jsTryOutcome {
+            continuation.resume(Outcome.Ok(result.toInt()))
+            (0).toJsNumber()
+        }.runOnError {
+            continuation.resume(Outcome.Error(it))
+        }
+    }
+    onerror = { continuation.resume(Outcome.Error(it)) }
+}
+
+suspend fun IDBRequest<JsAny>.await(): Outcome<JsAny?, Any> = suspendCoroutine { continuation ->
+    onsuccess = {
+        jsTryOutcome {
+            continuation.resume(Outcome.Ok(result))
+            (0).toJsNumber()
+        }.runOnError {
+            continuation.resume(Outcome.Error(it))
+        }
+    }
+    onerror = { continuation.resume(Outcome.Error(it)) }
+}
+
+suspend fun <T> IDBRequest<JsAny>.await(mapper: (JsAny) -> T): Outcome<T, Any> = suspendCoroutine { continuation ->
+    onsuccess = {
+        jsTryOutcome {
+            continuation.resume(Outcome.Ok(mapper(result)))
+            (0).toJsNumber()
+        }.runOnError {
+            continuation.resume(Outcome.Error(it))
+        }
+    }
+    onerror = { continuation.resume(Outcome.Error(it)) }
+}
+
+external interface IDBOpenSuccessEvent : JsAny {
+    val target: IDBRequestEventTarget
+}
+
+external class Event : JsAny {
+    val error: Error?
+    val target: JsAny
+}
+
+external class Error : JsAny {
+    val message: String
+}
+
+external class IDBVersionChangeEvent : JsAny {
+    val oldVersion: Int
+    val newVersion: Int
+    val target: IDBRequestEventTarget
+}
+
+external interface IDBRequestEventTarget : JsAny {
+    val result: IDBDatabase
+}
+
+external class IDBDatabase : JsAny {
+    fun createObjectStore(name: String): IDBObjectStore
+    fun createObjectStore(name: String, options: JsAny): IDBObjectStore
+    fun deleteObjectStore(name: String)
+    fun transaction(storeName: String): IDBTransaction
+    fun transaction(storeName: String, mode: String): IDBTransaction
+    fun transaction(storeNames: JsArray<JsString>): IDBTransaction
+    fun transaction(storeNames: JsArray<JsString>, mode: String): IDBTransaction
+    fun transaction(storeNames: JsArray<JsString>, mode: String, options: JsAny): IDBTransaction
+}
+
+fun createObjectStoreOptions(keyPath: String, autoIncrement: Boolean): JsAny = js(
+    """({keyPath: keyPath, autoIncrement: autoIncrement})""",
+)
+fun createObjectStoreOptions(keyPath: String): JsAny = js("""({keyPath: keyPath})""")
+fun createObjectStoreOptions(autoIncrement: Boolean): JsAny = js("""({autoIncrement: autoIncrement})""")
+
+external class IDBObjectStore : JsAny {
+    val keyPath: String?
+    val autoIncrement: Boolean
+    val name: String
+    val transaction: IDBTransaction
+
+    fun add(value: JsAny): IDBRequest<JsString>
+    fun add(value: JsAny, key: String): IDBRequest<JsString>
+    fun clear(): IDBRequest<JsAny>
+    fun count(): IDBRequest<JsNumber>
+    fun count(query: String): IDBRequest<JsNumber>
+    fun count(query: IDBKeyRange): IDBRequest<JsNumber>
+    fun createIndex(indexName: String, keyPath: String): IDBIndex
+    fun createIndex(indexName: String, keyPath: String, options: JsAny): IDBIndex
+    fun delete(key: String): IDBRequest<JsAny>
+    fun delete(key: IDBKeyRange): IDBRequest<JsAny>
+    fun deleteIndex(indexName: String)
+    fun get(key: String): IDBRequest<JsAny>
+    fun get(key: IDBKeyRange): IDBRequest<JsAny>
+    fun getKey(key: String): IDBRequest<JsString>
+    fun getKey(key: IDBKeyRange): IDBRequest<JsString>
+    fun getAll(): IDBRequest<JsAny>
+    fun getAllKeys(): IDBRequest<JsAny>
+    fun index(name: String): IDBIndex
+    fun openCursor(): IDBRequest<JsAny>
+    fun openKeyCursor(): IDBRequest<JsAny>
+    fun put(item: JsAny): IDBRequest<JsString>
+    fun put(item: JsAny, key: String): IDBRequest<JsString>
+}
+
+external class IDBTransaction : JsAny {
+    val db: IDBDatabase
+    val mode: String
+    var oncomplete: (JsAny) -> Unit
+    var onerror: (JsAny) -> Unit
+    var onabort: (JsAny) -> Unit
+    fun abort()
+    fun commit()
+    fun objectStore(name: String): IDBObjectStore
+}
+
+external class IDBIndex : JsAny {
+    val name: String
+    val keyPath: String
+    val objectStore: IDBObjectStore
+    val multiEntry: Boolean
+    val unique: Boolean
+}
+
+external class IDBKeyRange : JsAny
