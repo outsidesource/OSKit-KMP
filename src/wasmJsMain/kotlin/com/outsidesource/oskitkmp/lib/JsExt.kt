a053860857bac8ea4f9cd03c276d7863e4af5451
Ryan Mitchener <ryan@outsidesource.com>
2024-12-02 22:53:23 -0500

    Added new Kotlin 2.1.0 features

--- src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/lib/JsExt.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/lib/JsExt.kt
@@ -2,18 +2,6 @@ package com.outsidesource.oskitkmp.lib
 
 import com.outsidesource.oskitkmp.outcome.Outcome
 
-fun List<String>.toJsArray(): JsArray<JsString> = JsArray<JsString>().apply {
-    this@toJsArray.forEachIndexed { i, string -> set(i, string.toJsString()) }
-}
-
-fun JsArray<JsString>.toSet(): Set<String> = buildSet {
-    apply {
-        for (i in 0 until length) {
-            get(i)?.let { add(it.toString()) }
-        }
-    }
-}
-
 @Suppress("UNCHECKED_CAST")
 fun <T : JsAny> jsTryOutcome(block: () -> T): Outcome<T, Any> {
     val result = jsTry(block = block)
