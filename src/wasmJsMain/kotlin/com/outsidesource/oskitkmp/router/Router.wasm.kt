dffc42062958fac2ae4b8a43a0609832b0dcd7ca
Ryan Mitchener <ryan@outsidesource.com>
2024-12-14 02:48:36 -0500

    Fixes

--- src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/router/Router.wasm.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/router/Router.wasm.kt
@@ -1,5 +1,6 @@
 package com.outsidesource.oskitkmp.router
 
+import com.outsidesource.oskitkmp.lib.printAll
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.CoroutineScope
@@ -7,7 +8,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.w3c.dom.PopStateEvent
 import org.w3c.dom.events.Event
@@ -15,38 +15,66 @@ import org.w3c.dom.events.Event
 private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
 // TODO: Need to allow cleanup of listeners
+// TODO: There are some bugs with the interplay of popBackStack and history.back/forward
+// TODO: Test replaces
 
 actual fun initForPlatform(router: Router) {
-    var updateState = true
+    var handleNewRoute = true
+    var handlePopState = true
     val routeCache = mutableListOf<RouteStackEntry>()
+    var currentIndex = -1
 
     scope.launch {
+        routeCache += router.current
+
         router.routeFlow.collect { entry ->
             if (entry.route !is IWebRoute) return@collect
-            if (!updateState) {
-                updateState = true
+            if (!handleNewRoute) {
+                handleNewRoute = true
                 return@collect
             }
-            entry.route.title?.let { document.title = it }
-            entry.route.path?.let { window.history.pushState(entry.id.toJsNumber(), entry.route.title ?: "", it) }
-            routeCache += entry
+
+            val newIndex = routeCache.indexOfFirst { it.id == entry.id }
+            printAll("handling route", currentIndex, newIndex)
+
+            when {
+                newIndex == -1 || newIndex > currentIndex -> {
+                    entry.route.title?.let { document.title = it }
+                    entry.route.path?.let {
+                        window.history.pushState(entry.id.toJsNumber(), entry.route.title ?: "", it)
+                    }
+                    routeCache += entry
+                    currentIndex = routeCache.size - 1
+                }
+                newIndex < currentIndex -> {
+                    handlePopState = false
+                    window.history.go(newIndex - currentIndex)
+                    currentIndex = newIndex
+                }
+            }
         }
     }
 
     scope.launch {
         popStateFlow().collect { ev ->
-            println(ev)
-            val currentIndex = routeCache.indexOfFirst { it.id == router.current.id }
+            if (!handlePopState) {
+                handlePopState = true
+                return@collect
+            }
+            currentIndex = routeCache.indexOfFirst { it.id == router.current.id }
             val newIndex = routeCache.indexOfFirst { it.id == (ev.state as? JsNumber?)?.toInt() }
-            if (newIndex == -1) return@collect
+            printAll("handling pop state", currentIndex, newIndex)
 
-            updateState = false
-            if (newIndex < currentIndex) router.popWhile { it != routeCache[newIndex].route }
-            if (newIndex > currentIndex) {
-                for (i in currentIndex + 1..newIndex) {
-                    router.push(routeCache[i].route, routeCache[i].transition, true)
+            handleNewRoute = false
+            when {
+                newIndex == -1 -> router.popWhile(force = true) { true }
+                newIndex < currentIndex -> router.popWhile(force = true) { it != routeCache[newIndex].route }
+                newIndex > currentIndex -> for (i in currentIndex + 1..newIndex) {
+                    router.push(routeCache[i].route, routeCache[i].transition, force = true)
                 }
             }
+
+            currentIndex = newIndex
         }
     }
 }
