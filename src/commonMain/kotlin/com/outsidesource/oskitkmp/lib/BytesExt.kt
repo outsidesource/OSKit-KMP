82f6c19bcbba75d292ba18c8c7529cd55be8b0bc
Ryan Mitchener <ryan@outsidesource.com>
2024-12-17 11:38:50 -0500

    Formatting fixes

--- src/commonMain/kotlin/com/outsidesource/oskitkmp/lib/BytesExt.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitkmp/lib/BytesExt.kt
@@ -23,11 +23,10 @@ fun UInt.reverse(): UInt {
 fun ByteArray.toShort(start: Int = 0): Short =
     (((this[start + 0].toInt() and 0xFF) shl 8) or (this[start + 1].toInt() and 0xFF)).toShort()
 
-fun ByteArray.toShortLe(start: Int = 0): Short =
-    (
-        (this[start + 0].toInt() and 0xFF) or
-            ((this[start + 1].toInt() and 0xFF) shl 8)
-        ).toShort()
+fun ByteArray.toShortLe(start: Int = 0): Short = (
+    (this[start + 0].toInt() and 0xFF) or
+        ((this[start + 1].toInt() and 0xFF) shl 8)
+    ).toShort()
 
 fun Short.toBytes(buffer: ByteArray = ByteArray(2), start: Int = 0): ByteArray {
     buffer[start + 0] = ((this.toInt() shr 8) and 0xFF).toByte()
@@ -87,17 +86,19 @@ fun Int.toBytesLe(buffer: ByteArray = ByteArray(4), start: Int = 0): ByteArray {
     return buffer
 }
 
-fun ByteArray.toUInt(start: Int = 0): UInt =
-    (((this[start + 0].toInt() and 0xFF) shl 24) or
-            ((this[start + 1].toInt() and 0xFF) shl 16) or
-            ((this[start + 2].toInt() and 0xFF) shl 8) or
-            (this[start + 3].toInt() and 0xFF)).toUInt()
+fun ByteArray.toUInt(start: Int = 0): UInt = (
+    ((this[start + 0].toInt() and 0xFF) shl 24) or
+        ((this[start + 1].toInt() and 0xFF) shl 16) or
+        ((this[start + 2].toInt() and 0xFF) shl 8) or
+        (this[start + 3].toInt() and 0xFF)
+    ).toUInt()
 
-fun ByteArray.toUIntLe(start: Int = 0): UInt =
-    ((this[start + 0].toInt() and 0xFF) or
-            ((this[start + 1].toInt() and 0xFF) shl 8) or
-            ((this[start + 2].toInt() and 0xFF) shl 16) or
-            ((this[start + 3].toInt() and 0xFF) shl 24)).toUInt()
+fun ByteArray.toUIntLe(start: Int = 0): UInt = (
+    (this[start + 0].toInt() and 0xFF) or
+        ((this[start + 1].toInt() and 0xFF) shl 8) or
+        ((this[start + 2].toInt() and 0xFF) shl 16) or
+        ((this[start + 3].toInt() and 0xFF) shl 24)
+    ).toUInt()
 
 fun UInt.toBytes(buffer: ByteArray = ByteArray(4), start: Int = 0): ByteArray {
     buffer[start + 0] = ((this shr 24) and 0xFFu).toByte()
@@ -159,25 +160,27 @@ fun Long.toBytesLe(buffer: ByteArray = ByteArray(8), start: Int = 0): ByteArray 
     return buffer
 }
 
-fun ByteArray.toULong(start: Int = 0): ULong =
-    (((this[start + 0].toLong() and 0xFF) shl 56) or
-            ((this[start + 1].toLong() and 0xFF) shl 48) or
-            ((this[start + 2].toLong() and 0xFF) shl 40) or
-            ((this[start + 3].toLong() and 0xFF) shl 32) or
-            ((this[start + 4].toLong() and 0xFF) shl 24) or
-            ((this[start + 5].toLong() and 0xFF) shl 16) or
-            ((this[start + 6].toLong() and 0xFF) shl 8) or
-            (this[start + 7].toLong() and 0xFF)).toULong()
+fun ByteArray.toULong(start: Int = 0): ULong = (
+    ((this[start + 0].toLong() and 0xFF) shl 56) or
+        ((this[start + 1].toLong() and 0xFF) shl 48) or
+        ((this[start + 2].toLong() and 0xFF) shl 40) or
+        ((this[start + 3].toLong() and 0xFF) shl 32) or
+        ((this[start + 4].toLong() and 0xFF) shl 24) or
+        ((this[start + 5].toLong() and 0xFF) shl 16) or
+        ((this[start + 6].toLong() and 0xFF) shl 8) or
+        (this[start + 7].toLong() and 0xFF)
+    ).toULong()
 
-fun ByteArray.toULongLe(start: Int = 0): ULong =
-    ((this[start + 0].toLong() and 0xFF) or
-            ((this[start + 1].toLong() and 0xFF) shl 8) or
-            ((this[start + 2].toLong() and 0xFF) shl 16) or
-            ((this[start + 3].toLong() and 0xFF) shl 24) or
-            ((this[start + 4].toLong() and 0xFF) shl 32) or
-            ((this[start + 5].toLong() and 0xFF) shl 40) or
-            ((this[start + 6].toLong() and 0xFF) shl 48) or
-            ((this[start + 7].toLong() and 0xFF) shl 56)).toULong()
+fun ByteArray.toULongLe(start: Int = 0): ULong = (
+    (this[start + 0].toLong() and 0xFF) or
+        ((this[start + 1].toLong() and 0xFF) shl 8) or
+        ((this[start + 2].toLong() and 0xFF) shl 16) or
+        ((this[start + 3].toLong() and 0xFF) shl 24) or
+        ((this[start + 4].toLong() and 0xFF) shl 32) or
+        ((this[start + 5].toLong() and 0xFF) shl 40) or
+        ((this[start + 6].toLong() and 0xFF) shl 48) or
+        ((this[start + 7].toLong() and 0xFF) shl 56)
+    ).toULong()
 
 fun ULong.toBytes(buffer: ByteArray = ByteArray(8), start: Int = 0): ByteArray {
     buffer[start + 0] = ((this shr 56) and 0xFFu).toByte()
