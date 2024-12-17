fca6d086c411cbab000981143d3c97818aaeb647
Ryan Mitchener <ryan@outsidesource.com>
2024-12-16 23:28:08 -0500

    Added unit tests for BytesExt

--- src/commonMain/kotlin/com/outsidesource/oskitkmp/lib/BytesExt.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitkmp/lib/BytesExt.kt
@@ -41,6 +41,24 @@ fun Short.toBytesLe(buffer: ByteArray = ByteArray(2), start: Int = 0): ByteArray
     return buffer
 }
 
+fun ByteArray.toUShort(start: Int = 0): UShort =
+    (((this[start + 0].toInt() and 0xFF) shl 8) or (this[start + 1].toInt() and 0xFF)).toUShort()
+
+fun ByteArray.toUShortLe(start: Int = 0): UShort =
+    ((this[start + 0].toInt() and 0xFF) or ((this[start + 1].toInt() and 0xFF) shl 8)).toUShort()
+
+fun UShort.toBytes(buffer: ByteArray = ByteArray(2), start: Int = 0): ByteArray {
+    buffer[start + 0] = ((this.toInt() shr 8) and 0xFF).toByte()
+    buffer[start + 1] = (this.toInt() and 0xFF).toByte()
+    return buffer
+}
+
+fun UShort.toBytesLe(buffer: ByteArray = ByteArray(2), start: Int = 0): ByteArray {
+    buffer[start + 0] = (this.toInt() and 0xFF).toByte()
+    buffer[start + 1] = ((this.toInt() shr 8) and 0xFF).toByte()
+    return buffer
+}
+
 fun ByteArray.toInt(start: Int = 0): Int =
     ((this[start + 0].toInt() and 0xFF) shl 24) or
         ((this[start + 1].toInt() and 0xFF) shl 16) or
@@ -66,6 +84,34 @@ fun Int.toBytesLe(buffer: ByteArray = ByteArray(4), start: Int = 0): ByteArray {
     buffer[start + 1] = ((this shr 8) and 0xFF).toByte()
     buffer[start + 2] = ((this shr 16) and 0xFF).toByte()
     buffer[start + 3] = ((this shr 24) and 0xFF).toByte()
+    return buffer
+}
+
+fun ByteArray.toUInt(start: Int = 0): UInt =
+    (((this[start + 0].toInt() and 0xFF) shl 24) or
+            ((this[start + 1].toInt() and 0xFF) shl 16) or
+            ((this[start + 2].toInt() and 0xFF) shl 8) or
+            (this[start + 3].toInt() and 0xFF)).toUInt()
+
+fun ByteArray.toUIntLe(start: Int = 0): UInt =
+    ((this[start + 0].toInt() and 0xFF) or
+            ((this[start + 1].toInt() and 0xFF) shl 8) or
+            ((this[start + 2].toInt() and 0xFF) shl 16) or
+            ((this[start + 3].toInt() and 0xFF) shl 24)).toUInt()
+
+fun UInt.toBytes(buffer: ByteArray = ByteArray(4), start: Int = 0): ByteArray {
+    buffer[start + 0] = ((this shr 24) and 0xFFu).toByte()
+    buffer[start + 1] = ((this shr 16) and 0xFFu).toByte()
+    buffer[start + 2] = ((this shr 8) and 0xFFu).toByte()
+    buffer[start + 3] = (this and 0xFFu).toByte()
+    return buffer
+}
+
+fun UInt.toBytesLe(buffer: ByteArray = ByteArray(4), start: Int = 0): ByteArray {
+    buffer[start + 0] = (this and 0xFFu).toByte()
+    buffer[start + 1] = ((this shr 8) and 0xFFu).toByte()
+    buffer[start + 2] = ((this shr 16) and 0xFFu).toByte()
+    buffer[start + 3] = ((this shr 24) and 0xFFu).toByte()
     return buffer
 }
 
@@ -110,6 +156,50 @@ fun Long.toBytesLe(buffer: ByteArray = ByteArray(8), start: Int = 0): ByteArray 
     buffer[start + 5] = ((this shr 40) and 0xFF).toByte()
     buffer[start + 6] = ((this shr 48) and 0xFF).toByte()
     buffer[start + 7] = ((this shr 56) and 0xFF).toByte()
+    return buffer
+}
+
+fun ByteArray.toULong(start: Int = 0): ULong =
+    (((this[start + 0].toLong() and 0xFF) shl 56) or
+            ((this[start + 1].toLong() and 0xFF) shl 48) or
+            ((this[start + 2].toLong() and 0xFF) shl 40) or
+            ((this[start + 3].toLong() and 0xFF) shl 32) or
+            ((this[start + 4].toLong() and 0xFF) shl 24) or
+            ((this[start + 5].toLong() and 0xFF) shl 16) or
+            ((this[start + 6].toLong() and 0xFF) shl 8) or
+            (this[start + 7].toLong() and 0xFF)).toULong()
+
+fun ByteArray.toULongLe(start: Int = 0): ULong =
+    ((this[start + 0].toLong() and 0xFF) or
+            ((this[start + 1].toLong() and 0xFF) shl 8) or
+            ((this[start + 2].toLong() and 0xFF) shl 16) or
+            ((this[start + 3].toLong() and 0xFF) shl 24) or
+            ((this[start + 4].toLong() and 0xFF) shl 32) or
+            ((this[start + 5].toLong() and 0xFF) shl 40) or
+            ((this[start + 6].toLong() and 0xFF) shl 48) or
+            ((this[start + 7].toLong() and 0xFF) shl 56)).toULong()
+
+fun ULong.toBytes(buffer: ByteArray = ByteArray(8), start: Int = 0): ByteArray {
+    buffer[start + 0] = ((this shr 56) and 0xFFu).toByte()
+    buffer[start + 1] = ((this shr 48) and 0xFFu).toByte()
+    buffer[start + 2] = ((this shr 40) and 0xFFu).toByte()
+    buffer[start + 3] = ((this shr 32) and 0xFFu).toByte()
+    buffer[start + 4] = ((this shr 24) and 0xFFu).toByte()
+    buffer[start + 5] = ((this shr 16) and 0xFFu).toByte()
+    buffer[start + 6] = ((this shr 8) and 0xFFu).toByte()
+    buffer[start + 7] = (this and 0xFFu).toByte()
+    return buffer
+}
+
+fun ULong.toBytesLe(buffer: ByteArray = ByteArray(8), start: Int = 0): ByteArray {
+    buffer[start + 0] = (this and 0xFFu).toByte()
+    buffer[start + 1] = ((this shr 8) and 0xFFu).toByte()
+    buffer[start + 2] = ((this shr 16) and 0xFFu).toByte()
+    buffer[start + 3] = ((this shr 24) and 0xFFu).toByte()
+    buffer[start + 4] = ((this shr 32) and 0xFFu).toByte()
+    buffer[start + 5] = ((this shr 40) and 0xFFu).toByte()
+    buffer[start + 6] = ((this shr 48) and 0xFFu).toByte()
+    buffer[start + 7] = ((this shr 56) and 0xFFu).toByte()
     return buffer
 }
 
---
+++ src/commonTest/kotlin/com/outsidesource/oskitkmp/lib/BytesExtTest.kt
@@ -0,0 +1,135 @@
+package com.outsidesource.oskitkmp.lib
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class BytesExtTest {
+    @Test
+    fun testShortConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x01)
+        val bytes2 = byteArrayOf(0x01, 0x00)
+        val value1 = 1.toShort()
+        val value2 = 256.toShort()
+        assertTrue("Conversion value 1") { bytes1.toShort() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toShort() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toShortLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toShortLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @Test
+    fun testUShortConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x80.toByte())
+        val bytes2 = byteArrayOf(0x80.toByte(), 0x00)
+        val value1 = 128u.toUShort()
+        val value2 = 32768u.toUShort()
+        assertTrue("Conversion value 1") { bytes1.toUShort() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toUShort() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toUShortLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toUShortLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @Test
+    fun testIntConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x00, 0x00, 0x01)
+        val bytes2 = byteArrayOf(0x01, 0x00, 0x00, 0x00)
+        val value1 = 1
+        val value2 = 16777216
+        assertTrue("Conversion value 1") { bytes1.toInt() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toInt() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toIntLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toIntLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @Test
+    fun testUIntConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x00, 0x00, 0x80.toByte())
+        val bytes2 = byteArrayOf(0x80.toByte(), 0x00, 0x00, 0x00)
+        val value1 = 128u
+        val value2 = 2147483648u
+        assertTrue("Conversion value 1") { bytes1.toUInt() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toUInt() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toUIntLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toUIntLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @Test
+    fun testLongConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)
+        val bytes2 = byteArrayOf(0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+        val value1 = 1L
+        val value2 = 72057594037927936L
+        assertTrue("Conversion value 1") { bytes1.toLong() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toLong() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toLongLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toLongLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @Test
+    fun testULongConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80.toByte())
+        val bytes2 = byteArrayOf(0x80.toByte(), 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+        val value1 = 128u.toULong()
+        val value2 = 9223372036854775808u
+        assertTrue("Conversion value 1") { bytes1.toULong() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toULong() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toULongLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toULongLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    @Test
+    fun testFloatConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x00, 0x00, 0x01)
+        val bytes2 = byteArrayOf(0x01, 0x00, 0x00, 0x00)
+        val value1 = 1e-45f
+        val value2 = 2.3509887e-38f
+        assertTrue("Conversion value 1") { bytes1.toFloat() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toFloat() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toFloatLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toFloatLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+
+    @Test
+    fun testDoubleConversion() {
+        val bytes1 = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)
+        val bytes2 = byteArrayOf(0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+        val value1 = 4.9E-324
+        val value2 = 7.2911220195563975E-304
+        assertTrue("Conversion value 1") { bytes1.toDouble() == value1 }
+        assertTrue("Conversion value 2") { bytes2.toDouble() == value2 }
+        assertTrue("Conversion value le 1") { bytes2.toDoubleLe() == value1 }
+        assertTrue("Conversion value le 2") { bytes1.toDoubleLe() == value2 }
+        assertTrue("Conversion to bytes 1") { value1.toBytes().contentEquals(bytes1) }
+        assertTrue("Conversion to bytes 2") { value2.toBytes().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 1") { value1.toBytesLe().contentEquals(bytes2) }
+        assertTrue("Conversion to bytes le 2") { value2.toBytesLe().contentEquals(bytes1) }
+    }
+}
