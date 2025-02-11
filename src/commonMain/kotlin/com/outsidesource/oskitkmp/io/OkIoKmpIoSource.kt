a065177ffe4686b0cdd686c2992ea33e2a9d41ee
Ryan Mitchener <ryan@outsidesource.com>
2025-02-11 11:04:49 -0500

    Fixed KtLint issue

--- src/commonMain/kotlin/com/outsidesource/oskitkmp/io/OkIoKmpIoSource.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitkmp/io/OkIoKmpIoSource.kt
@@ -7,11 +7,8 @@ class OkIoKmpIoSource(source: Source) : IKmpIoSource {
     private val buffer = source.buffer()
 
     override suspend fun require(byteCount: Long) = buffer.require(byteCount)
-    override suspend fun read(buffer: ByteArray, bufferOffset: Int, byteCount: Int): Int = this@OkIoKmpIoSource.buffer.read(
-        buffer,
-        bufferOffset,
-        byteCount,
-    )
+    override suspend fun read(buffer: ByteArray, bufferOffset: Int, byteCount: Int): Int =
+        this@OkIoKmpIoSource.buffer.read(buffer, bufferOffset, byteCount)
     override suspend fun readUtf8Line(buffer: ByteArray): String? = this@OkIoKmpIoSource.buffer.readUtf8Line()
     override suspend fun readAll(): ByteArray = buffer.readByteArray()
     override suspend fun close() = buffer.close()
