a94adce457780720fc57a257a34b1305477cbbd2
Dmitri Livotov <dmitri.livotov@ontarget-group.com>
2025-06-25 20:36:38 +0500

    implement notification capability check

--- src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/capability/LocalNotifications.wasm.kt.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitkmp/capability/LocalNotifications.wasm.kt.kt
@@ -1,24 +1,62 @@
 package com.outsidesource.oskitkmp.capability
 
 import com.outsidesource.oskitkmp.outcome.Outcome
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import org.w3c.notifications.Notification
+import org.w3c.notifications.NotificationPermission
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-class LocalNotificationsKmpCapability() : IInitializableKmpCapability, IKmpCapability {
-    override fun init(context: KmpCapabilityContext) {}
+private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
-    override val status: Flow<CapabilityStatus> = flow { emit(queryStatus()) }
-    override val hasPermissions: Boolean = false
+class LocalNotificationsKmpCapability : IInitializableKmpCapability, IKmpCapability {
+
+    private val localStatusFlow = MutableStateFlow<CapabilityStatus>(
+        if (!hardwareSupportsCapability()) {
+            CapabilityStatus.Unsupported()
+        } else {
+            CapabilityStatus.Unknown
+        },
+    )
+
+    override val status: Flow<CapabilityStatus> = localStatusFlow
+
+    override val hasPermissions: Boolean = true
     override val hasEnablableService: Boolean = false
     override val supportsRequestEnable: Boolean = false
     override val supportsOpenAppSettingsScreen: Boolean = false
     override val supportsOpenServiceSettingsScreen: Boolean = false
 
-    override suspend fun queryStatus(): CapabilityStatus =
-        CapabilityStatus.Unsupported(UnsupportedReason.NotImplemented)
+    override fun init(context: KmpCapabilityContext) {
+        scope.launch {
+            if (!hardwareSupportsCapability()) return@launch
 
-    override suspend fun requestPermissions(): Outcome<CapabilityStatus, Any> =
-        Outcome.Error(KmpCapabilitiesError.UnsupportedOperation)
+            val permission = Notification.permission
+            localStatusFlow.value = mapPermissionToCapabilityStatus(permission)
+        }
+    }
+
+    override suspend fun queryStatus(): CapabilityStatus {
+        if (!hardwareSupportsCapability()) return CapabilityStatus.Unsupported()
+        return mapPermissionToCapabilityStatus(Notification.permission)
+    }
+
+    override suspend fun requestPermissions(): Outcome<CapabilityStatus, Any> {
+        if (!hardwareSupportsCapability()) return Outcome.Error(KmpCapabilitiesError.UnsupportedOperation)
+
+        return suspendCoroutine { continuation ->
+            Notification.requestPermission { permission ->
+                val status = mapPermissionToCapabilityStatus(permission)
+                localStatusFlow.value = status
+                continuation.resume(Outcome.Ok(status))
+            }
+        }
+    }
 
     override suspend fun requestEnable(): Outcome<CapabilityStatus, Any> =
         Outcome.Error(KmpCapabilitiesError.UnsupportedOperation)
@@ -28,4 +66,15 @@ class LocalNotificationsKmpCapability() : IInitializableKmpCapability, IKmpCapab
 
     override suspend fun openAppSettingsScreen(): Outcome<Unit, Any> =
         Outcome.Error(KmpCapabilitiesError.UnsupportedOperation)
+
+    private fun mapPermissionToCapabilityStatus(permission: NotificationPermission): CapabilityStatus {
+        return when (permission.toString()) {
+            "granted" -> CapabilityStatus.Ready
+            "default" -> CapabilityStatus.NoPermission(NoPermissionReason.NotRequested)
+            "denied" -> CapabilityStatus.NoPermission(NoPermissionReason.DeniedPermanently)
+            else -> CapabilityStatus.Unknown
+        }
+    }
 }
+
+private fun hardwareSupportsCapability(): Boolean = js("""typeof Notification !== undefined""")
