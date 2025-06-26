420e8d3cf219924920fe167cae5974fe690e9c0a
Ryan Mitchener <ryan@outsidesource.com>
2025-06-26 11:15:23 -0400

    Updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:8.1.2")
+                useModule("com.android.tools.build:gradle:8.10.1")
             }
         }
     }
