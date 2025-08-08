c530582b40101c07bf703b73ffe7ba4838ebbb7a
Ryan Mitchener <ryan@outsidesource.com>
2025-08-07 22:58:22 -0400

    added measureTimePrinted

--- src/commonMain/kotlin/com/outsidesource/oskitkmp/lib/Printed.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitkmp/lib/Printed.kt
@@ -1,5 +1,14 @@
 package com.outsidesource.oskitkmp.lib
 
+import kotlinx.datetime.Clock
+
 fun <T : Any?> T.printed(): T = apply { println(this) }
 
 fun printAll(vararg args: Any?) = println(args.joinToString(", "))
+
+suspend inline fun <T> measureTimePrinted(tag: String? = null, block: suspend () -> T): T {
+    val start = Clock.System.now()
+    val result = block()
+    println("Time Elapsed${if (tag != null) " - $tag - " else ""}${(Clock.System.now() - start).inWholeMilliseconds}ms")
+    return result
+}
