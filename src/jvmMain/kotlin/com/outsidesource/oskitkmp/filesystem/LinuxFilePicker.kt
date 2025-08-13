0e46bbfb3e15b3e7c5dc08679b2ad24ee58981c4
Ryan Mitchener <ryan@outsidesource.com>
2025-08-13 00:22:06 -0400

    continued work on LinuxFilePicker

--- src/jvmMain/kotlin/com/outsidesource/oskitkmp/filesystem/LinuxFilePicker.kt
+++ src/jvmMain/kotlin/com/outsidesource/oskitkmp/filesystem/LinuxFilePicker.kt
@@ -15,42 +15,27 @@ import org.freedesktop.dbus.types.Variant
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
+/**
+ * A DBus desktop portal implementation for file pickers
+ * https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.FileChooser.html
+ */
 @OptIn(ExperimentalUuidApi::class)
 class LinuxFilePicker(private val context: () -> KmpFsContext?) : IKmpFsFilePicker {
 
     override suspend fun pickFile(startingDir: KmpFsRef?, filter: KmpFileFilter?): Outcome<KmpFsRef?, KmpFsError> {
-        return DBusConnectionBuilder.forSessionBus().build().use { conn ->
-            val handleToken = Uuid.random().toString().replace("-", "")
-            val windowId = Native.getWindowID(context()?.window).toString()
-            val dbusObj = conn.getRemoteObject(
-                "org.freedesktop.portal.Desktop",
-                "/org/freedesktop/portal/desktop",
-                FileChooserInterface::class.java,
-            )
-
-            val deferred = CompletableDeferred<Unit>()
-            val matchRule = DBusMatchRule("signal", "org.freedesktop.portal.Request", "Response")
-            val handler = DBusSigHandler<DBusSignal> { signal ->
-                if (signal?.path?.endsWith(handleToken) != true) return@DBusSigHandler
-                val response = (signal.parameters[0] as UInt32).toInt()
-                println(response)
-                val results = signal.parameters[1] as Map<String, Variant<Any>>
-                println(results)
-                deferred.complete(Unit)
-            }
-            conn.addGenericSigHandler(matchRule, handler)
-            dbusObj.OpenFile(
+        val result = withDbusInterface<FileChooser> { windowId, handleToken ->
+            OpenFile(
                 windowId,
                 "",
                 mutableMapOf(
                     "handle_token" to Variant(handleToken),
                 ),
             )
-            deferred.await()
-            conn.removeGenericSigHandler(matchRule, handler)
-
-            Outcome.Ok(null)
         }
+
+        println(result.results["uris"])
+
+        return Outcome.Ok(null)
     }
 
     override suspend fun pickFiles(
@@ -68,8 +53,44 @@ class LinuxFilePicker(private val context: () -> KmpFsContext?) : IKmpFsFilePick
         TODO("Not yet implemented")
     }
 
+    private suspend inline fun <reified T : DBusInterface> withDbusInterface(
+        interfaceCall: T.(windowId: String, handleToken: String) -> Unit,
+    ): Response {
+        return DBusConnectionBuilder.forSessionBus().build().use { conn ->
+            val handleToken = Uuid.random().toString().replace("-", "")
+            val windowId = Native.getWindowID(context()?.window).toString()
+            val dbusObj = conn.getRemoteObject(
+                "org.freedesktop.portal.Desktop",
+                "/org/freedesktop/portal/desktop",
+                T::class.java,
+            )
+
+            val deferred = CompletableDeferred<Response>()
+            val matchRule = DBusMatchRule("signal", "org.freedesktop.portal.Request", "Response")
+            val handler = DBusSigHandler<DBusSignal> { signal ->
+                if (signal?.path?.endsWith(handleToken) != true) return@DBusSigHandler
+                val response = Response(
+                    code = (signal.parameters[0] as UInt32).toInt(),
+                    results = signal.parameters[1] as Map<String, Variant<Any>>,
+                )
+                deferred.complete(response)
+            }
+            conn.addGenericSigHandler(matchRule, handler)
+
+            dbusObj.interfaceCall(windowId, handleToken)
+            val response = deferred.await()
+            conn.removeGenericSigHandler(matchRule, handler)
+            response
+        }
+    }
+
+    private data class Response(
+        val code: Int,
+        val results: Map<String, Variant<Any>>,
+    )
+
     @DBusInterfaceName(value = "org.freedesktop.portal.FileChooser")
-    internal interface FileChooserInterface : DBusInterface {
+    private interface FileChooser : DBusInterface {
         fun OpenFile(
             parentWindow: String,
             title: String,
