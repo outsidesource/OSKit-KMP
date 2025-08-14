4930767a86310cbd7f0b1e026260746c914a9d9d
Ryan Mitchener <ryan@outsidesource.com>
2025-08-13 23:01:26 -0400

    Finished Linux file picker

--- src/jvmMain/kotlin/com/outsidesource/oskitkmp/filesystem/LinuxFilePicker.kt
+++ src/jvmMain/kotlin/com/outsidesource/oskitkmp/filesystem/LinuxFilePicker.kt
@@ -5,13 +5,18 @@ import com.sun.jna.Native
 import kotlinx.coroutines.CompletableDeferred
 import org.freedesktop.dbus.DBusMatchRule
 import org.freedesktop.dbus.DBusPath
+import org.freedesktop.dbus.Tuple
 import org.freedesktop.dbus.annotations.DBusInterfaceName
+import org.freedesktop.dbus.annotations.Position
 import org.freedesktop.dbus.connections.impl.DBusConnectionBuilder
 import org.freedesktop.dbus.interfaces.DBusInterface
 import org.freedesktop.dbus.interfaces.DBusSigHandler
 import org.freedesktop.dbus.messages.DBusSignal
 import org.freedesktop.dbus.types.UInt32
 import org.freedesktop.dbus.types.Variant
+import java.net.URI
+import kotlin.io.path.name
+import kotlin.io.path.toPath
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -27,30 +32,75 @@ class LinuxFilePicker(private val context: () -> KmpFsContext?) : IKmpFsFilePick
             OpenFile(
                 windowId,
                 "",
-                mutableMapOf(
-                    "handle_token" to Variant(handleToken),
-                ),
+                mutableMapOf<String, Variant<*>>().apply {
+                    this["handle_token"] = Variant(handleToken)
+                    addFileFilter(filter)
+                    addStartDirectory(startingDir)
+                },
             )
         }
 
-        println(result.results["uris"])
-
-        return Outcome.Ok(null)
+        if (result.code != 0) return Outcome.Ok(null)
+        val refs = getRefsFromResponse(result, isDirectory = false)?.firstOrNull()
+        return Outcome.Ok(refs)
     }
 
     override suspend fun pickFiles(
         startingDir: KmpFsRef?,
         filter: KmpFileFilter?,
     ): Outcome<List<KmpFsRef>?, KmpFsError> {
-        TODO("Not yet implemented")
+        val result = withDbusInterface<FileChooser> { windowId, handleToken ->
+            OpenFile(
+                windowId,
+                "",
+                mutableMapOf<String, Variant<*>>().apply {
+                    this["handle_token"] = Variant(handleToken)
+                    this["multiple"] = Variant(true)
+                    addFileFilter(filter)
+                    addStartDirectory(startingDir)
+                },
+            )
+        }
+
+        if (result.code != 0) return Outcome.Ok(null)
+        val refs = getRefsFromResponse(result, isDirectory = false)
+        return Outcome.Ok(refs)
     }
 
     override suspend fun pickDirectory(startingDir: KmpFsRef?): Outcome<KmpFsRef?, KmpFsError> {
-        TODO("Not yet implemented")
+        val result = withDbusInterface<FileChooser> { windowId, handleToken ->
+            OpenFile(
+                windowId,
+                "",
+                mutableMapOf<String, Variant<*>>().apply {
+                    this["handle_token"] = Variant(handleToken)
+                    this["directory"] = Variant(true)
+                    addStartDirectory(startingDir)
+                },
+            )
+        }
+
+        if (result.code != 0) return Outcome.Ok(null)
+        val refs = getRefsFromResponse(result, isDirectory = true)?.firstOrNull()
+        return Outcome.Ok(refs)
     }
 
     override suspend fun pickSaveFile(fileName: String, startingDir: KmpFsRef?): Outcome<KmpFsRef?, KmpFsError> {
-        TODO("Not yet implemented")
+        val result = withDbusInterface<FileChooser> { windowId, handleToken ->
+            SaveFile(
+                windowId,
+                "",
+                mutableMapOf<String, Variant<*>>().apply {
+                    this["handle_token"] = Variant(handleToken)
+                    this["current_name"] = Variant(fileName)
+                    addStartDirectory(startingDir)
+                },
+            )
+        }
+
+        if (result.code != 0) return Outcome.Ok(null)
+        val refs = getRefsFromResponse(result, isDirectory = false)?.firstOrNull()
+        return Outcome.Ok(refs)
     }
 
     private suspend inline fun <reified T : DBusInterface> withDbusInterface(
@@ -84,6 +134,32 @@ class LinuxFilePicker(private val context: () -> KmpFsContext?) : IKmpFsFilePick
         }
     }
 
+    private fun MutableMap<String, Variant<*>>.addFileFilter(filter: KmpFileFilter?) = apply {
+        if (filter == null) return@apply
+        this["filters"] = Variant(
+            listOf(Pair("Supported Files", filter.map { Pair(0, "*.${it.extension}") })),
+            "a(sa(us))",
+        )
+    }
+
+    private fun MutableMap<String, Variant<*>>.addStartDirectory(startingDir: KmpFsRef?) = apply {
+        if (startingDir == null) return@apply
+        this["current_folder"] = Variant(startingDir.ref.toByteArray() + 0x00)
+    }
+
+    private fun getRefsFromResponse(result: Response, isDirectory: Boolean): List<KmpFsRef>? {
+        val uris = (result.results["uris"]?.value as? List<String>) ?: return null
+        return uris.map {
+            val path = URI(it).toPath()
+            KmpFsRef(
+                ref = path.toString(),
+                name = path.name,
+                fsType = KmpFsType.External,
+                isDirectory = isDirectory,
+            )
+        }
+    }
+
     private data class Response(
         val code: Int,
         val results: Map<String, Variant<Any>>,
@@ -102,11 +178,10 @@ class LinuxFilePicker(private val context: () -> KmpFsContext?) : IKmpFsFilePick
             title: String,
             options: MutableMap<String, Variant<*>>,
         ): DBusPath
-
-        fun SaveFiles(
-            parentWindow: String,
-            title: String,
-            options: MutableMap<String, Variant<*>>,
-        ): DBusPath
     }
+
+    private class Pair<T1, T2>(
+        @field:Position(0) val first: T1,
+        @field:Position(1) val second: T2,
+    ) : Tuple()
 }
